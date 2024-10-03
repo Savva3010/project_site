83c452e556856bf376dbdd2c9f8da0644659d416
Savva <71602412+Savva3010@users.noreply.github.com>
2024-10-03 22:01:30 +0300

    header
    added header + included bootstrap-icons

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,7 +1,17 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import css from "@/styles/header.module.css"
 
 export default function Header() {
   return (
-    <header>
+    <header className={`${css["header"]}`}>
+      <div className={`${css["title"]}`}>
+        <a href="https://cabinet.ftl.name" className={`${css["title-txt"]}`}>Кабинет</a>
+        <span className={`bi bi-layout-text-sidebar`}></span>
+      </div>
+      <div className={`${css["profile"]}`}>
+        <p>Серебренников Савва</p>
+        <span className={`bi bi-person-circle`}></span>
+      </div>
     </header>
   )
 }
