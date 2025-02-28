11ec9a1b1dfed210eed29f3e34c7b6fa3b9829c4
Ok :/ <71602412+Savva3010@users.noreply.github.com>
2025-02-28 21:51:50 +0300

    added dockerfile

--- temp_server/index.js
+++ temp_server/index.js
@@ -38,7 +38,7 @@ app.use(bodyParser.raw())
 app.use(fileUpload())
 app.use(methodOverride("_method"))
 app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', ['http://localhost:3000'])
+    res.set('Access-Control-Allow-Origin', "*")
     res.set("Access-Control-Allow-Methods", "*")
     res.set("Access-Control-Allow-Headers", ["Content-Type", "Key", "Value"])
     next()
