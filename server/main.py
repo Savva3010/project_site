0183d2581dd760cc39bac74360de2cd1120c193b
KenSinKan <demmax0512@gmail.com>
2025-03-01 09:48:31 +0300

    filenames

--- server/main.py
+++ server/main.py
@@ -1044,6 +1044,7 @@ async def get_leave_application(
         FROM application_files
         WHERE application_id = ?
     ''', (application_id,)).fetchall()
+    
 
     conn.close()
 
@@ -1079,7 +1080,7 @@ async def get_leave_application(
         "status": app_data['status'],
         "comment": app_data['comment'],
         "created_at": app_data['created_at'],
-        "files": [dict(file) for file in files]
+        "files": ['/' + dict(file)['src'][9:] for file in files]
     }
 
     return {"success": True, "data": formatted_response}
